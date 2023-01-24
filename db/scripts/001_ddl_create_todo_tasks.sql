bbc26f15cd287f532aa5690ae9dc3bbff5d67a2a
Fut <shmopls@gmail.com>
2023-01-24 14:35:07 +0500

    FIXED: Comments in scripts

--- db/scripts/001_ddl_create_todo_tasks.sql
+++ db/scripts/001_ddl_create_todo_tasks.sql
@@ -6,7 +6,7 @@ CREATE TABLE if NOT EXISTS todo_tasks (
 );
 
 comment on table todo_tasks is 'Задачи';
-comment on column tasks.id is 'Уникальный идентификатор задачи';
-comment on column tasks.description is 'Описание задачи';
-comment on column tasks.created is 'Время создания задачи';
-comment on column tasks.done is 'Статус исполнения задачи';
+comment on column todo_tasks.id is 'Уникальный идентификатор задачи';
+comment on column todo_tasks.description is 'Описание задачи';
+comment on column todo_tasks.created is 'Время создания задачи';
+comment on column todo_tasks.done is 'Статус исполнения задачи';
--- db/scripts/002_ddl_todo_user.sql
+++ db/scripts/002_ddl_todo_user.sql
@@ -8,9 +8,9 @@ CREATE TABLE if NOT EXISTS todo_users (
 );
 
 comment on table todo_users is '”четна€ запись пользовател€';
-comment on column users.id is '”никальный идентификатор учетной записи пользовател€';
-comment on column users.name is '»м€ учетной записи пользовател€';
-comment on column users.email is 'Ёл.почта учетной записи пользовател€';
-comment on column users.login is '”никальный логин учетной записи пользовател€';
-comment on column users.password is 'ѕароль учетной записи пользовател€';
-comment on column users.created is '¬рем€ создани€ учетной записи пользовател€';
+comment on column todo_users.id is '”никальный идентификатор учетной записи пользовател€';
+comment on column todo_users.name is '»м€ учетной записи пользовател€';
+comment on column todo_users.email is 'Ёл.почта учетной записи пользовател€';
+comment on column todo_users.login is '”никальный логин учетной записи пользовател€';
+comment on column todo_users.password is 'ѕароль учетной записи пользовател€';
+comment on column todo_users.created is '¬рем€ создани€ учетной записи пользовател€';
